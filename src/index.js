7023f1a10bf48a9806ad61bdda817441920c86d0
uilegendsteam@gmail.com <uilegendsteam@gmail.com>
2021-08-12 14:05:22 +0530

    change in app

--- src/index.js
+++ src/index.js
@@ -1,96 +1,125 @@
-import React, { Component } from 'react'
-import ReactDom from 'react-dom';
-import AddTodo from './AddTodo';
-import TodoItem from './TodoItem';
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+import AddTodo from "./AddTodo";
+import TodoItem from "./TodoItem";
+import axios from "axios";
+import { URL } from "./Constants";
 export class Index extends Component {
+  // Define state
+  state = {
+    todos: [],
+  };
 
- // Define state
- state = {
-  todos: [
-   {
-    text: "Buy Milk",
-    completed: true
-   },
-   {
-    text: "Buy Egg",
-    completed: false
-   }
-  ]
- }
+  componentDidMount() {
+    axios.get(`${URL}`).then((result) => {
+      this.setState({
+        todos: result.data,
+      });
+    });
+  }
 
- // Change state
- addTodoToState = (text) => {
-  const newTodo = this.state.todos.concat({
-   text,
-   completed: false
-  })
-  this.setState({
-   todos: newTodo
-  })
- }
+  // Change state for adding
+  addTodoToState = (text) => {
+    axios
+      .post(`${URL}`, {
+        text,
+        completed: false,
+      })
+      .then((result) => {
+        const newTodo = this.state.todos.concat({
+          text,
+          completed: false,
+        });
+        this.setState({
+          todos: newTodo,
+        });
+      });
+  };
 
- deleteTodoList = (index) => {
-  const newTodo = this.state.todos.filter((todo, i) => {
-   return index === i ? false : true;
-  });
-  this.setState({
-   todos: newTodo
-  })
- }
+  deleteTodoList = (index) => {
+    const { todos } = this.state;
+    const todo = todos[index];
+    axios.delete(`${URL}${todo.id}`).then((res) => {
+      const newTodo = todos.filter((todo, i) => {
+        return index === i ? false : true;
+      });
+      this.setState({
+        todos: newTodo,
+      });
+    });
+  };
 
- editTodofromState = (index, newText) => {
-  const newTodos = this.state.todos.map((todo, i) => {
-   if (index === i) {
-    return {
-     ...todo,
-     text: newText
-    }
-   }
-   return todo;
-  })
-  this.setState({
-   todos: newTodos
-  })
- }
+  editTodofromState = (index, newText) => {
+    const { todos } = this.state;
+    const todo = todos[index];
+    axios.put(`${URL}${todo.id}`, { ...todo, text: newText }).then((result) => {
+      const newTodos = todos.map((todo, i) => {
+        if (index === i) {
+          return {
+            ...todo,
+            text: newText,
+          };
+        }
+        return todo;
+      });
+      this.setState({
+        todos: newTodos,
+      });
+    });
+  };
 
+  toggleComplete = (index) => {
+    const { todos } = this.state;
+    const todo = todos[index];
+    const newTodo = todos.map((todo, i) => {
+      if (index === i) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+      return todo;
+    });
 
- toggleComplete = (index) => {
-  const newTodo = this.state.todos.map((todo, i) => {
-   if (index === i) {
-    return {
-     ...todo,
-     completed:!todo.completed
-     }
-   }
-   return todo;
-  })
-  this.setState({
-   todos: newTodo
-  })
- }
- 
- render() {
-  return (
-   <div>
-    <ul>
-     {this.state.todos.map((todo, index) => {
-      return (
-       <TodoItem
-        editTodofromState={this.editTodofromState}
-        deleteTodoList={this.deleteTodoList}
-        toggleComplete={this.toggleComplete}
-        todo={todo} index={index}  key={index} />
-      )
-     })}
-    </ul>
-    <AddTodo addTodoToState={this.addTodoToState} />
-   </div>
-  )
- }
+    axios
+      .put(`${URL}${todo.id}`, { ...todo, completed: !todo.completed })
+      .then((res) => {
+        this.setState({
+          todos: newTodo,
+        });
+      });
+  };
+  // Unmounted
+  componentWillUnmount() {
+    // fix Warning: Can't perform a React state update on an unmounted component
+    this.setState = (state, callback) => {
+      return;
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map((todo, index) => {
+            return (
+              <TodoItem
+                editTodofromState={this.editTodofromState}
+                deleteTodoList={this.deleteTodoList}
+                toggleComplete={this.toggleComplete}
+                todo={todo}
+                index={index}
+                key={index}
+              />
+            );
+          })}
+        </ul>
+        <AddTodo addTodoToState={this.addTodoToState} />
+      </div>
+    );
+  }
 }
 
-export default Index
+export default Index;
 
-
-ReactDom.render(<Index />, document.querySelector('#root'));
-
+ReactDom.render(<Index />, document.querySelector("#root"));
